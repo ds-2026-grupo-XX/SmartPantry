b348fd5b4bbcd9da81928eb1b791e7c1e7a42eab
Mario <mario.caballero.948@gmail.com>
2026-09-17 15:40:48 -0300

    se agregan las constantes de producto campos requeridos

--- aspnet-core/src/SmartPantry.Domain/Product/Product.entity.cs
+++ aspnet-core/src/SmartPantry.Domain/Product/Product.entity.cs
@@ -1,13 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Volo.Abp.Domain.Entities;
 
 namespace SmartPantry;
 
 public class Product : BasicAggregateRoot<Guid>
 {
+  [Required]
+  [MaxLength(ProductConst.MaxCodDeBarraLength)]
   public string CodigoDeBarras { get; set; } = string.Empty;
+
+  [Required]
+  [MaxLength(ProductConst.MaxNombreVisibleLength)]
   public string NombreVisible { get; set; } = string.Empty;
+
   public string Imagen { get; set; } = string.Empty;
+
   public float NutriScore { get; set; } = 0;
-  public int Nova { get; set; } = 0;
+
+  [MaxLength(ProductConst.MaxNovaGroup)]
+  [MinLength(ProductConst.MinNovaGroup)]
+  public int Nova { get; set; } = 1;
 }
