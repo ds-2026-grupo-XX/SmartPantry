a04c5af29a203c0b33e3ee8470b21a36e59a0d97
Juan <juan.coletti@gmail.com>
2026-09-23 19:43:10 -0300

    se agrega CRUD a la entidad

---
+++ aspnet-core/src/SmartPantry.EntityFrameworkCore/Migrations/20260923224131_UpDate_Product.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartPantry.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpDate_Product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Borrado",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Borrado",
+                table: "Products");
+        }
+    }
+}
